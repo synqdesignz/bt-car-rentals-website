f4b72c250fe0c649efb8fe78d6b4a168d550c15b
synqdesignz <synzdesignz@gmail.com>
2025-04-02 20:54:34 -0400

    Correct Email For Admin.

--- btrentals/settings.py
+++ btrentals/settings.py
@@ -103,7 +103,7 @@ EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "apikey")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "bt-rentals246@hotmail.com")
 
 
 # Password validation
