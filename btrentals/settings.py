ca84c0321b5a45c79a88b110e4b22b9263d71166
synqdesignz <synqdesignz@gmail.com>
2025-07-30 00:12:07 -0400

    Minor Changes for Database Connection.

--- btrentals/settings.py
+++ btrentals/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT', '4000'),
         'OPTIONS':  db_options,
         }
